6a94c3f1f2e892fedf64c0851dedce54c0273b56
NoNamedeo <meelalej@gmail.com>
2026-04-01 18:17:13 +0200

    added structure files

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='SEF',
+    version='0.1.0',
+    description='Signal extraction framework',
+    url='https://github.com/NoNamedeo/SEF',
+    author='Alejandro Innocenzi, Matteo Vittori',
+    author_email='meelalej@gmail.com'
+    '''
+    license='BSD 2-clause',
+    packages=['pyexample'],
+    install_requires=['mpi4py>=2.0',
+                      'numpy',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    '''
+)
