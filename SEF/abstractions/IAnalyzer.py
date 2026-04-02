6ba5ea4a60a4c90de250203e007dbe8bc6dccd5a
NoNamedeo <meelalej@gmail.com>
2026-04-02 10:19:20 +0200

    added artifacts and utils classes

--- SEF/abstractions/IAnalyzer.py
+++ SEF/abstractions/IAnalyzer.py
@@ -1,11 +1,14 @@
 from abc import ABC, abstractmethod
 from typing import Any, Dict
 
+from SEF.artifacts.Signal import Signal
+from SEF.artifacts.Data import Data
+
 class IAnalyzer(ABC):
 
     def __init__(self, config: Dict[str, Any] | None = None):
         self.config = config or {}
 
     @abstractmethod
-    def analyze(self, signal):
+    def analyze(self, signal: Signal) -> Data:
         pass
